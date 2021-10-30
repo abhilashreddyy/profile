2a3d1123e04bfa4c99d6314906b75d7dd5d3c571
abhilashreddyy <abhilashreddyy1@gmail.com>
2021-10-29 20:08:37 -0700

    started changing

---
+++ my_profile/src/data/contact.js
@@ -0,0 +1,34 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faKaggle } from '@fortawesome/free-brands-svg-icons/faKaggle';
+
+// import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+// See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
+// to add other icons.
+
+const data = [
+  {
+    link: 'https://github.com/abhilashreddyy',
+    label: 'Github',
+    icon: faGithub,
+  },
+  {
+    link: 'https://www.facebook.com/abhilashreddy.yammanuru',
+    label: 'Facebook',
+    icon: faFacebookF,
+  },
+  {
+    link: 'https://www.linkedin.com/in/yabhilashreddy/',
+    label: 'LinkedIn',
+    icon: faLinkedinIn,
+  },
+  {
+    link: 'https://www.kaggle.com/abhilashreddyy',
+    label: 'Kaggle',
+    icon: faKaggle,
+  }
+
+];
+
+export default data;
